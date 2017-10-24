94908bd1d57a57dde2b64e16b49a0ffd1c047cf7
Filip Stangricki <stangricki@gmail.com>
2017-10-24 20:04:26 +0200

    Field calculation

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+var a=prompt("Define base (a) of a traingle"), 
+	h=prompt("Define height (h) of a traingle"), 
+	triangleArea;
+triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
